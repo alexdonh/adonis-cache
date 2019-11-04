541966f00816dfc5a264e6b1689c4ba759cc8937
Alex Do <alex.donh@gmail.com>
2019-11-04 23:08:11 +0700

    implement ExpressionDependency

--- src/Dependencies/Expression.js
+++ src/Dependencies/Expression.js
@@ -1,9 +1,18 @@
 'use strict'
 
+const safeeval = require('safe-eval')
 const Dependency = require('./Dependency')
 
 class ExpressionDependency extends Dependency {
+  constructor (config) {
+    super(config)
+    this.expression = config.expression || 'true'
+    this.params = config.params || {}
+  }
 
+  async _generateDependencyData (cache) {
+    return safeeval(this.expression)
+  }
 }
 
 module.exports = ExpressionDependency
