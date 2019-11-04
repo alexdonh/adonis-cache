4d2dc21ad3926f722056c7d5cb07e28370aadb20
Alex Do <alex.donh@gmail.com>
2019-11-04 23:19:10 +0700

    implement ChainedDependency

--- src/Dependencies/Chained.js
+++ src/Dependencies/Chained.js
@@ -3,7 +3,33 @@
 const Dependency = require('./Dependency')
 
 class ChainedDependency extends Dependency {
+  constructor (config) {
+    super(config)
+    this.dependencies = config.dependencies || []
+    this.dependOnAll = config.dependOnAll || true
+  }
 
+  async evaluateDependency (cache) {
+    for (const dep of this.dependencies) {
+      await dep.evaluateDependency(cache)
+    }
+  }
+
+  async _generateDependencyData (cache) {
+    return null
+  }
+
+  async isChanged (cache) {
+    for (const dep of this.dependencies) {
+      const isChanged = await dep.isChanged(cache)
+      if (this.dependOnAll && isChanged) {
+        return true
+      } else if (!this.dependOnAll && !isChanged) {
+        return false
+      }
+    }
+    return !this.dependOnAll
+  }
 }
 
 module.exports = ChainedDependency
