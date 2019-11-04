de737a4c38eb7c0fbb4a3d39d013321b5389982e
Alex Do <alex.donh@gmail.com>
2019-11-04 23:25:28 +0700

    implement FileDependency

--- src/Dependencies/File.js
+++ src/Dependencies/File.js
@@ -1,9 +1,23 @@
 'use strict'
 
+const fs = require('fs-extra')
+const { RuntimeException } = require('@adonisjs/generic-exceptions')
 const Dependency = require('./Dependency')
 
 class FileDependency extends Dependency {
+  constructor (config) {
+    super(config)
+    this.fileName = config.fileName || false
+  }
 
+  async _generateDependencyData (cache) {
+    if (!this.fileName) {
+      throw RuntimeException.invoke('File name must be set')
+    }
+    return fs.stat(this.fileName)
+      .then(stats => stats.mtime || false)
+      .catch(() => false)
+  }
 }
 
 module.exports = FileDependency
